22f3a0e5f3185e2dedbea92f0008dd424aed308c
Caden Preece <caden.preece@gmail.com>
2021-04-20 18:42:50 -0600

    caden

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,74 +1,60 @@
 
+//***** ROUTES ********
+// Routes that show before Login
+// Index, about, services, Login/Signup
+
+//begining of services routes
+exports.showconstruction = (req, res) => {
+  res.render("./services/construction");
+}
+exports.showelectrician = (req, res) => {
+  res.render("./services/electrician");
+}
+exports.showmechanic = (req, res) => {
+  res.render("./services/mechanic");
+}
+exports.showplumbing = (req, res) => {
+  res.render("./services/plumbing");
+}
+exports.showtransportation = (req, res) => {
+  res.render("./services/transportation")
+}
+// //ending of services routes
 
 
-exports.showMyProject = (req, res) => {
-    res.render("myProject");
-  };
-  
-exports.showAbout = (req, res) => {
-  res.render("about");
-};
 
-exports.showConstruction = (req, res) => {
-  res.render("construction");
-};
-
-exports.showElectrician = (req, res) => {
-  res.render("electrician");
-};
-
-exports.showInbox = (req, res) => {
-  res.render("inbox");
-};
-
-exports.showIndex = (req, res) => {
+exports.index = (req,res) => {
   res.render("index");
-};
-
-exports.showInvoices = (req, res) => {
-  res.render("invoices");
-};
-
-exports.showLayout = (req, res) => {
-  res.render("layout");
-};
-
-exports.showLogin = (req, res) => {
-  res.render("login");
-};
-
-exports.showLoginSignUp = (req, res) => {
-  res.render("loginSignUp");
-};
-
-exports.showMechanics = (req, res) => {
-  res.render("mechanics");
-};
-
-exports.showPlumbing = (req, res) => {
-  res.render("plumbing");
-};
-
+}
+exports.about = (req,res) => {
+  res.render("about");
+}
+exports.searchServices = (req,res) => {
+  res.render("services");
+}
+exports.showSignIn = (req, res) => {
+    res.render("./signUp/login");
+  }
+exports.signUpPage = (req, res) => {
+  //res.render("loginSignUp");
+  res.render("./signUp/signUp");
+}
+exports.users = (req, res) => {
+  res.render("users");
+}
+//Routes that show after Login
+//Profile, Projects, Invoice, Inbox
 exports.showProfile = (req, res) => {
   res.render("profile");
-};
-
-exports.showServices = (req, res) => {
-  res.render("services");
-};
-
-exports.showTrucking = (req, res) => {
-  res.render("trucking");
-};
-
-
-
+}
+exports.showProjects = (req, res) => {
+  res.render("myProjects");
+}
+exports.showInvoices = (req, res) => {
+  res.render("invoices");
+}
+exports.showInbox = (req, res) => {
+  res.render("inbox");
+}
 
 
-
-
-
-
-
-
-  //home, courses, contact
